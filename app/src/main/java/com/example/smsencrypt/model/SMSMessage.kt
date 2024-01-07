562a5e92fabb7221b83e86f204644f56e0259f3d
speed150 <97861636+speed150@users.noreply.github.com>
2024-01-07 21:38:12 +0100

    encrypt

--- app/src/main/java/com/example/smsencrypt/model/SMSMessage.kt
+++ app/src/main/java/com/example/smsencrypt/model/SMSMessage.kt
@@ -18,6 +18,20 @@ fun Long.parseDate():String{
     val format = SimpleDateFormat("dd/MMM/yyyy HH:mm", Locale.getDefault())
     return format.format(date)
 }
+fun encrypt( mess :String):String{
+    var cypher=""
+    for (ch in mess){
+        cypher+=(ch+4)
+    }
+    return cypher
+}
+fun decrypt(cypher: String):String{
+    var mess=""
+    for (ch in cypher){
+        mess+=(ch-4)
+    }
+    return mess
+}
 //TODO model wszystkich wiadomości
 //TODO viewmodel posortowanych wiadomości(jest już w MainScreen prawie gotowy)
 //TODO wiewmodel rozmów(SMSMessage.thread)[ostatniawiadomość z danegej rozmowy]
