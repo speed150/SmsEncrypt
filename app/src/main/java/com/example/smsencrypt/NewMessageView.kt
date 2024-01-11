111250f0971c3352969eb5f72739ad1ba698fd20
speed150 <97861636+speed150@users.noreply.github.com>
2024-01-11 15:58:21 +0100

    Iconbutton

--- app/src/main/java/com/example/smsencrypt/NewMessageView.kt
+++ app/src/main/java/com/example/smsencrypt/NewMessageView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -15,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.smsencrypt.components.SMSTextBar
 
@@ -30,13 +32,19 @@ fun NewMessageView() {
                 .padding(5.dp),
             Arrangement.SpaceAround
         ) {
+            val pattern = remember { Regex("^\\d+\$") }
             OutlinedTextField(value =RNumber,
-                onValueChange ={RNumber=it},
+                onValueChange ={
+                    if (it.isEmpty() || it.matches(pattern)) {
+                    RNumber = it
+                    }
+                               },
                 placeholder = {
                     Text(
                         text = "Wpisz Numer Telefonu"
                     )
-                }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
             )
         }
     },
--- app/src/main/java/com/example/smsencrypt/components/SMSTextBar.kt
+++ app/src/main/java/com/example/smsencrypt/components/SMSTextBar.kt
@@ -3,6 +3,7 @@ package com.example.smsencrypt.components
 import android.content.Context
 import android.telephony.SmsManager
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,10 +12,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,8 +28,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.smsencrypt.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,16 +51,19 @@ fun SMSTextBar(number :String) {
         OutlinedTextField(modifier = Modifier.fillMaxWidth(0.75F), shape= RoundedCornerShape(25), placeholder = { Text(
             text = "SMS"
         )} , value = text, onValueChange ={text=it}, maxLines = 4)
-        Button(shape = RoundedCornerShape(30), modifier = Modifier.size(55.dp), onClick = {SendMessage(nr,text, context = context)}) {
-            Icon(Icons.Default.ArrowForward, contentDescription = "Send")
-        }
-
-        }
+            IconButton(onClick = {SendMessage(nr,text, context = context)},
+                Modifier.size(55.dp).clip(RoundedCornerShape(40))
+                    .background(MaterialTheme.colorScheme.primary)
+            ) {
+                Icon(Icons.Rounded.ArrowForward, contentDescription = "Send")
+            }
+         }
     }
 
     
 
 fun SendMessage(phoneNumber: String, message: String,context:Context) {
+
     if (phoneNumber.length != 9){
         Toast.makeText(context, "błędny numer telefonu", Toast.LENGTH_SHORT).show()
     }
